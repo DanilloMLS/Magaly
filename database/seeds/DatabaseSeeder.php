2201ebedb6ff2dd25b19e34baea0debb5582a45c
JSouza <j.otavio.ts@gmail.com>
2020-02-03 12:09:56 -0300

    Padronizando os ícones e adicionando novamente os seeders em ordem

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,13 +14,14 @@ class DatabaseSeeder extends Seeder
         //É necessário referenciar aqui cada seeder que será executado
         $this->call(UserTableSeeder::class);
         $this->call(EstoqueTableSeeder::class);
+        $this->call(ItensTableSeeder::class);
         $this->call(EscolaTableSeeder::class);
         $this->call(FornecedorTableSeeder::class);
-        $this->call(ContratoTableSeeder::class);
         $this->call(RefeicaoTableSeeder::class);
-        $this->call(ItensTableSeeder::class);
+        $this->call(ContratoTableSeeder::class);
         $this->call(ContratoItemTableSeeder::class);
         $this->call(Refeicao_ItemTableSeeder::class);
+        $this->call(EstoqueItemTableSeeder::class);
 
     }
 }
